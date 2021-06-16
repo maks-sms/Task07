2f8c8f62eca6534192ed44d654ba6794f4e3e81d
Maks <maks988ss@gmail.com>
2021-06-16 16:29:27 +0300

    Completing the fourth task

--- src/Color.java
+++ src/Color.java
@@ -1,3 +1,8 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
+
 public enum Color {
 
     RED("#ff0000"),
@@ -7,9 +12,38 @@ public enum Color {
     BLACK("#000000");
 
     private String hex;
+    private int r;
+    private int g;
+    private int b;
 
     Color(String hex) {
         this.hex = hex;
+        String regex = "#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(hex);
+
+        matcher.find();
+        String group = matcher.group(1);
+        String group2 = matcher.group(2);
+        String group3 = matcher.group(3);
+
+        r = parseInt(group, 16);
+        g = parseInt(group2, 16);
+        b = parseInt(group3, 16);
 
     }
+
+    Color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+
+        String r1 = Integer.toString(r, 16);
+        String g1 = Integer.toString(g, 16);
+        String b1 = Integer.toString(b, 16);
+
+        hex = "#" + r1 + g1 + b1;
+    }
+
 }
